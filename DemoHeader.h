c57dc45528a9fb5b259258158f1aaa57859ad638
zhang xiong <1025679612@qq.com>
2016-03-31 16:35:20 +0800

    Added files via upload

---
+++ DemoHeader.h
@@ -0,0 +1,27 @@
+
+
+#ifndef __ZX_OPENGL_DEMO_HEADER_H__
+#define __ZX_OPENGL_DEMO_HEADER_H__
+
+
+#include "../redbook/chap1/AppChap1_1.h"
+#include "../redbook/chap2/BufferObject.h"
+#include "../redbook/chap3/PointSize.h"
+#include "../redbook/chap3/DrawInstance.h"
+#include "../redbook/chap4/MRTDemo.h"
+#include "../redbook/chap4/AppChap1_4.h"
+#include "../redbook/Chap6/ChessBoard.h"
+#include "../redbook/Chap6/ProxyTexture.h"
+#include "../redbook/Chap6/SamplerDemo.h"
+#include "../redbook/Chap6/SwizzleDemo.h"
+#include "../redbook/Chap6/MultiTexture.h"
+#include "../redbook/Chap6/MultiTextureCC.h"
+#include "../redbook/Chap6/Texture3D.h"
+#include "../redbook/Chap6/SkyBox.h"
+#include "../redbook/Chap6/TextureMip.h"
+#include "../redbook/Chap6/PointSprite.h"
+
+
+#include "../redbook/Chap7/DoubeFaceShading.h"
+
+#endif /* __ZX_OPENGL_DEMO_HEADER_H__ */
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <memory>
+#include "NXShader.h"
+#include "NXLog.h"
+#include "NXVector.h"
+#include "NXMatrix.h"
+#include "DemoHeader.h"
+
+int main(int argc, const char* argv[]){
+    NX::glb_GetLog().logToConsole("begin main");
+    std::auto_ptr<NX::Application> app(new AppChap1_1());
+    if(!app->Init(argv, argc, 800, 800)){
+        std::cout << "failed init application..." << std::endl;
+        return 1;
+    }
+    NX::glb_GetLog().logToConsole("begin application");
+    app->Run();
+    NX::glb_GetLog().logToConsole("end main");
+    return 0;
+}
