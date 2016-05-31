f648c1bca3a98499e0dcf28f5718043e4200a885
张雄 <zhangxiong@MacBook-Pro.local>
2016-06-01 00:06:18 +0800

    view frustum

--- engine/render/NXViewFrustum.h
+++ engine/render/NXViewFrustum.h
@@ -2,15 +2,26 @@
  *  File:    NXViewFrustum.h
  *  author:  张雄
  *  date:    2016_06_01
- *  purpose: 定义ViewFrustum
+ *  purpose: 定义ViewFrustum及相应的一些操作函数
  */
 
 #ifndef __ZX_NXENGINE_VIEWFRUSTUM_H__
 #define __ZX_NXENGINE_VIEWFRUSTUM_H__
 
+#include "../math/NXPrimitive.h"
+
 namespace NX {
     class ViewFrustum{
-        
+    public:
+        ViewFrustum();
+        ~ViewFrustum();
+    private:
+        Plane     m_FrontPlane;
+        Plane     m_BackPlane;
+        Plane     m_LeftPlane;
+        Plane     m_RightPlane;
+        Plane     m_TopPlane;
+        Plane     m_BottomPlane;
     };
 }
 
