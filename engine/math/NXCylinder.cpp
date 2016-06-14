abb8f8e42218232f46026e97535a1d98040d07de
zhang <1025679612@qq.com>
2016-06-14 19:58:46 +0800

    review code

--- engine/math/NXCylinder.cpp
+++ engine/math/NXCylinder.cpp
@@ -23,7 +23,6 @@ namespace NX{
         Transform(R);
     }
     
-    
     Cylinder::Cylinder(const float fLongAxis, const float fShortAxis, const float fHeight, const NX::Matrix<float, 4, 4> &M):m_vCenter(0.0f, 0.0f, 0.0f), m_vNormal(0.0f, 1.0f, 0.0f), m_vLongAxis(1.0f, 0.0f, 0.0f), m_vShortAxis(0.0f, 0.0f, 1.0f){
         m_fLongAxis   = fLongAxis;
         m_fShortAxis  = fShortAxis;
